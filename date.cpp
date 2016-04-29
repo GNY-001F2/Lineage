925b3c7f4aea33ffcf1f8b3bac0cd5c7582c9d92
Aayush Agarwal <aayush.agarwal_wu@yahoo.com>
2016-04-29 16:34:07 -0400

    added licensing info, more code

--- date.cpp
+++ date.cpp
@@ -1,46 +1,98 @@
+/* date.cpp; reference implementation for a date class
+ * Copyright (C) 2016  Aayush Agarwal
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
 #include "date.h"
 namespace date {
     
-/* date_t(std::string rawdate): constructs the date from the input date string
- * 
- * string rawdate - the date in the format YYYYMMDD
+/* constructs the date from the input date string
+ * string rawdate - the date in the format [CE/BCE]-YYYY-MM-DD
  */
 date_t::date_t(std::string rawdate): datecontainer(0u) {
     
+    validate(rawdate);
+    uint8_t day = 0u, month = 0u;
+    uint32_t year = 0u;
     /*TODO: take the rawdate string and convert it into its constituent parts*/
     //implementation here
     /*TODO: call the setter functions*/
-//     setday(day);
-//     setmtnh(month);
-//     setyr(yr);
+    setday(day);
+    setmnth(month);
+    setyr(year);
     /*TODO: validate the date*/
 //     validate(datecontainer);
 
 }
+
+/* sets the day bits in datecontainer
+ * uint8_t day - the day
+ * does not return anything
+ */
 void date_t::setday(uint8_t day) {
     
-    uint32_t day32 = (uint32_t) day;
+    if (day == 0u) day++;
+    uint32_t day32 = static_cast<uint32_t>(day);
     datecontainer = day32;
     return;
 }
+
+/* sets the month bits in datecontainer
+ * uint8_t mnth - the month
+ * does not return anything
+ */
 void date_t::setmnth(uint8_t month) {
     
+    if (month == 0u) month++;
     uint32_t mnth32 = static_cast<uint32_t>(month) << 4;
     datecontainer |= mnth32;
     return;
 }
+
+/* sets the year bits in datecontainer
+ * uint32_t year - the year
+ * does not return anything
+ */
 void date_t::setyr(uint32_t year) {
     
+    if (year == 0u) year++;
     datecontainer |= static_cast<uint32_t>(year) << 8;
     /*TODO: implement code to set year as leap year */
     return;
 }
-void date_t::setbce(unsigned char bce) {
+
+/* sets if the date happened before common era or is in the common era
+ * bool bce - true means CE, false means BCE
+ * does not return anything
+ */
+void date_t::setbce(bool bce) {
     
     uint32_t bce32 = static_cast<uint32_t>(bce) << 30;
     datecontainer |= bce32;
     return;
 }
+
+/* checks if the string is correctly entered
+ * 
+ */
+int date_t::validate(std::string rawdate) {
+    /*TODO: check if the date is being constructed correctly*/
+    //if date string is ill-formed, throw exception
+    return stoi(rawdate); //placeholder
+}
+
 void date_t::validate(uint32_t datecontainer) {
     
     /* since, we are only concerned with the values stored in the first
--- date.h
+++ date.h
@@ -1,8 +1,26 @@
+/* date.h; definitions for the date structure
+ * Copyright (C) 2016  Aayush Agarwal
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
 #ifndef DATE_H
 #define DATE_H
 
 #include <cinttypes>
 #include <string>
+#include <memory>
 
 /*date bitmasks*/
 #define DAYM ((uint32_t) 0x1Fu)
@@ -30,9 +48,16 @@ private:
     void setday(uint8_t day);
     void setmnth(uint8_t month);
     void setyr(uint32_t year);
-    void setbce(unsigned char bce);
+    void setbce(bool bce);
+    int validate(std::string rawdate);
     void validate(uint32_t datecontainer);
+    
+friend
+    void increment(std::weak_ptr<date_t> datecontainer, std::string currdate);
 };
+
+//needed for game clock
+void increment(std::weak_ptr<date_t> datecontainer, std::string currdate);
 }
 
 #endif
