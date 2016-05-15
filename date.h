bd118d6b4088a5a0be895577ee016b57e5e6ba65
Aayush Agarwal <aayush.agarwal_wu@yahoo.com>
2016-05-15 01:34:52 -0400

    updated date.h specifications

--- date.h
+++ date.h
@@ -1,18 +1,18 @@
 /* date.h; definitions for the date structure
  * Copyright (C) 2016  Aayush Agarwal
  *
- * This program is free software; you can redistribute it and/or modify
+ * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; version 2 of the License.
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef DATE_H
@@ -21,43 +21,81 @@
 #include <cinttypes>
 #include <string>
 #include <memory>
-
-/*date bitmasks*/
-#define DAYM ((uint32_t) 0x1Fu)
-#define MONM ((uint32_t) 0x1E0u)
-#define YRM (~((uint32_t) 0x1FFu))
-#define BAN_DATE (~((uint32_t) 0x7FFFFFFFu))
-#define DATE_VALID (1 << 31)
+#include <array>
 
 namespace date {
 
-/** structure contains an unsigned 32 bit integer that stores 
- * the day in 5 bits, the month in the next 4 bits.
- * leap year designation set in the 30th bit.
- * all bits between these reserved bits can be used to store a year.
- * CE or BCE designation is stored in the 31st bit.
- * Date validity is stored in the 32nd bit.
+/** date_t contains an unsigned 32 bit integer that stores the year in bits
+ * 0 to 15, the month in bits 16 to 19, day in bits 20 to 24 the leap year
+ * status in bit 25, CE or BCE designation is stored in bit 26 and date
+ * validity in bit 31. The remaining bits are left to the user of this class
+ * to use for their own purposes.
+ *
+ * data members:
+ *
+ * uint32_t datecontainer - Stores the date with above specification
+ *
+ * constructors:
+ *
+ * date_t(uint32_t datecontainer) - Sets datecontainer with the value of
+ * rawdate, with a default of 0. this constructor should call validity()
+ * to set the valid bit, and throw an exception if the date is invalid
+ * and the default value of 0u is not used. An object constructed with
+ * the value 0u should initialize this structure correctly with
+ * one of the date_init() methods.
+ *
+ * methods:
+ *
+ * int date_init() - Collection of functions to initialize the date structure
+ *
+ ** (std::string ISO_date, std::string era = "CE", bool suppressinval = false
+ * std::string delimiter = '-') initialize directly from an ISO 8061 format
+ * date string
+ *
+ ** (uint32_t rawyr, uint32_t rawmnth, uint32_t rawday, std::string era = "CE",
+ * bool suppressinval = false) initialize by supplying numeric values for date
+ * month and year
+ *
+ ** (uint32_t rawdate, bool suppressinval = false) similar to calling the
+ * constructor, but it lets the calling program/function ignore the validity
+ * of the entered date value.
+ *
+ *** bool suppressinval suppresses the date validity check, and is false by
+ * default. However it does not suppress the setting of the valid bit.
+ *
+ * void validate() - checks if the date entered is valid or not. handling
+ * invalidation is left to the user
+ * std::string get_ISO_date() - returns the date in an ISO 8061 format string
+ * std::array<std::string, 2> get_ISO_date_and_Era() - returns a string array
+ * with the first element containing the era the second an ISO 8061 format date
+ * and another the era. Example: {"1987-04-22", "CE"}
  */
-typedef struct date_t date_t;
-struct date_t {
+
+// typedef struct date_t date_t;
+typedef struct date_t {
 
     uint32_t datecontainer;
-    date_t(std::string rawdate);
 
-private:
-    void setday(uint8_t day);
-    void setmnth(uint8_t month);
-    void setyr(uint32_t year);
-    void setbce(bool bce);
-    int validate(std::string rawdate);
-    void validate(uint32_t datecontainer);
-    
-friend
-    void increment(std::weak_ptr<date_t> datecontainer, std::string currdate);
-};
+    date_t(uint32_t datecontainer = 0u);
 
-//needed for game clock
-void increment(std::weak_ptr<date_t> datecontainer, std::string currdate);
+    int date_init(std::string ISO_date, std::string era = "CE",
+                  bool suppressinval = false, std::string delimiter = '-');
+    int date_init(uint32_t rawyr, uint32_t rawmnth, uint32_t rawday,
+                  std::string era = "CE", bool suppressinval = false);
+    int date_init(uint32_t rawdate, bool suppressinval = false);
+//     void setday(uint8_t day);
+//     void setmnth(uint8_t month);
+//     void setyr(uint32_t year);
+//     void setbce(bool bce);
+//     int validate(std::string rawdate);
+
+public:
+    void validate(bool suppressinval = false);
+    std::string get_ISO_date();
+    std::array<std::string, 2> get_ISO_date_and_era();
+
+} date_t;
+
 }
 
 #endif
